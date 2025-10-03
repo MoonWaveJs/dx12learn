201cddde3bd2e352d3fe159ff9deddfdf771ec6a
jiangshang1994 <503455071@qq.com>
2025-10-03 16:51:40 +0800

    fbx环境完善

--- FBXLoader/FBXLoader.cpp
+++ FBXLoader/FBXLoader.cpp
@@ -3,8 +3,152 @@
 
 #include "pch.h"
 #include "framework.h"
+#include "FBXLoader.h"
 
-// TODO: 这是一个库函数示例
-void fnFBXLoader()
+#pragma region output helper
+
+
+/* Tab character ("\t") counter */
+int numTabs = 0;
+
+/**
+ * Print the required number of tabs.
+ */
+void PrintTabs() {
+    for (int i = 0; i < numTabs; i++)
+        printf("\t");
+}
+
+/**
+ * Return a string-based representation based on the attribute type.
+ */
+FbxString GetAttributeTypeName(FbxNodeAttribute::EType type) {
+    switch (type) {
+    case FbxNodeAttribute::eUnknown: return "unidentified";
+    case FbxNodeAttribute::eNull: return "null";
+    case FbxNodeAttribute::eMarker: return "marker";
+    case FbxNodeAttribute::eSkeleton: return "skeleton";
+    case FbxNodeAttribute::eMesh: return "mesh";
+    case FbxNodeAttribute::eNurbs: return "nurbs";
+    case FbxNodeAttribute::ePatch: return "patch";
+    case FbxNodeAttribute::eCamera: return "camera";
+    case FbxNodeAttribute::eCameraStereo: return "stereo";
+    case FbxNodeAttribute::eCameraSwitcher: return "camera switcher";
+    case FbxNodeAttribute::eLight: return "light";
+    case FbxNodeAttribute::eOpticalReference: return "optical reference";
+    case FbxNodeAttribute::eOpticalMarker: return "marker";
+    case FbxNodeAttribute::eNurbsCurve: return "nurbs curve";
+    case FbxNodeAttribute::eTrimNurbsSurface: return "trim nurbs surface";
+    case FbxNodeAttribute::eBoundary: return "boundary";
+    case FbxNodeAttribute::eNurbsSurface: return "nurbs surface";
+    case FbxNodeAttribute::eShape: return "shape";
+    case FbxNodeAttribute::eLODGroup: return "lodgroup";
+    case FbxNodeAttribute::eSubDiv: return "subdiv";
+    default: return "unknown";
+    }
+}
+
+/**
+ * Print an attribute.
+ */
+void PrintAttribute(FbxNodeAttribute* pAttribute) {
+    if (!pAttribute) return;
+
+    FbxString typeName = GetAttributeTypeName(pAttribute->GetAttributeType());
+    FbxString attrName = pAttribute->GetName();
+    PrintTabs();
+    // Note: to retrieve the character array of a FbxString, use its Buffer() method.
+    printf("<attribute type='%s' name='%s'/>\n", typeName.Buffer(), attrName.Buffer());
+}
+
+/**
+ * Print a node, its attributes, and all its children recursively.
+ */
+void PrintNode(FbxNode* pNode) {
+    PrintTabs();
+    const char* nodeName = pNode->GetName();
+    FbxDouble3 translation = pNode->LclTranslation.Get();
+    FbxDouble3 rotation = pNode->LclRotation.Get();
+    FbxDouble3 scaling = pNode->LclScaling.Get();
+
+    // Print the contents of the node.
+    printf("<node name='%s' translation='(%f, %f, %f)' rotation='(%f, %f, %f)' scaling='(%f, %f, %f)'>\n",
+        nodeName,
+        translation[0], translation[1], translation[2],
+        rotation[0], rotation[1], rotation[2],
+        scaling[0], scaling[1], scaling[2]
+    );
+    numTabs++;
+
+    // Print the node's attributes.
+    for (int i = 0; i < pNode->GetNodeAttributeCount(); i++)
+        PrintAttribute(pNode->GetNodeAttributeByIndex(i));
+
+    // Recursively print the children.
+    for (int j = 0; j < pNode->GetChildCount(); j++)
+        PrintNode(pNode->GetChild(j));
+
+    numTabs--;
+    PrintTabs();
+    printf("</node>\n");
+}
+#pragma endregion
+
+static std::shared_ptr<FbxLoader> Instance;
+
+
+FbxLoader::FbxLoader()
+{
+    lSdkManager = FbxManager::Create();
+    lImporter = FbxImporter::Create(lSdkManager, "");
+}
+
+FbxLoader::~FbxLoader()
+{
+    lImporter->Destroy();
+    lSdkManager->Destroy();
+    Instance.reset();
+    printf("FbxLoader Destroy./n");
+}
+
+void FbxLoader::FnFbxLoaderInit()
 {
 }
+
+void FbxLoader::FnOutputScene(std::string fbx)
+{
+    bool lImportStatus = lImporter->Initialize(fbx.c_str(), -1);	// -1 表示让fbxsdk根据文件的扩展名去判断文件的格式
+    if (!lImportStatus)
+    {
+        printf("Call to FbxImporter::Initialize() failed./n");
+        printf("Error returned: %s/n/n", lImporter->GetStatus().GetErrorString());
+        exit(-1);
+    }
+
+    FbxScene* scene = FbxScene::Create(lSdkManager, "");
+    lImporter->Import(scene);
+    FbxNode* lRootNode = scene->GetRootNode();
+    if (lRootNode) {
+        for (int i = 0; i < lRootNode->GetChildCount(); i++)
+            PrintNode(lRootNode->GetChild(i));
+    }
+    lRootNode->Destroy();
+    scene->Destroy();
+}
+
+
+void FbxLoader::Release()
+{
+    Instance.reset();
+}
+
+
+std::shared_ptr<FbxLoader> GetFbxLoader()
+{
+    if (!Instance)
+    {
+        Instance = std::make_shared<FbxLoader>();
+    }
+    return Instance;
+}
+
---
+++ FBXLoader/FBXLoader.h
@@ -0,0 +1,42 @@
+#pragma once
+#include <string>
+#include <fbxsdk.h>
+#include <memory>
+
+__declspec(dllexport) struct VertexData
+{
+    float tx;
+    float ty;
+    float tz;
+
+    float rx;
+    float ry;
+    float rz;
+
+    float sx;
+    float sy;
+    float sz;
+
+    
+};
+
+__declspec(dllexport) class FbxLoader
+{
+public:
+    __declspec(dllexport) void FnFbxLoaderInit();
+    __declspec(dllexport) void FnOutputScene(std::string fbx);
+
+    __declspec(dllexport) void FnGetVertexGroup();
+    
+    __declspec(dllexport) void Release();
+    ~FbxLoader();
+    FbxLoader();
+
+private:
+    FbxManager* lSdkManager;
+    FbxImporter* lImporter;
+private:
+
+};
+
+std::shared_ptr<FbxLoader> _declspec(dllexport) GetFbxLoader();
